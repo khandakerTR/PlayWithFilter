51c8ed008ec1bff4f82d3fe770bfd0f2babb64f9
Tushar Khandaker <khandaker3047@gmail.com>
2022-07-20 01:49:25 +0600

    Initial Commit

---
+++ PlayWithFilter/CustomCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCollectionViewCell.swift
+//  PlayWithFilter
+//
+//  Created by Tushar Khandaker on 7/20/22.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
--- PlayWithFilter/ViewController.swift
+++ PlayWithFilter/ViewController.swift
@@ -6,14 +6,58 @@
 //
 
 import UIKit
+import CoreImage
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var filterCollectionView: UICollectionView!
+    @IBOutlet weak var imageView: UIImageView! {
+        didSet {
+            imageView.image = UIImage(named: "sample.jpg", in: Bundle(for: type(of: self)), compatibleWith: nil)
+        }
+    }
+    let filters = ["CIGaussianBlur","CIGaussianBlur","CIGaussianBlur","CIGaussianBlur","CIGaussianBlur","CIGaussianBlur","CIGaussianBlur","CIGaussianBlur","CIGaussianBlur","CIGaussianBlur","CIGaussianBlur","CIGaussianBlur","CIGaussianBlur","CIGaussianBlur","CIGaussianBlur","CIGaussianBlur"]
+    
+    var context = CIContext()
+    var currentFilter = "CISepiaTone"
+    var currentImage = UIImage()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
+        currentImage = UIImage(named: "sample.jpg", in: Bundle(for: type(of: self)), compatibleWith: nil)!
     }
-
-
 }
 
+extension ViewController {
+    
+    func applyFilter() {
+        let ciImage = CIImage(image: currentImage)
+        let filter = CIFilter(name: currentFilter)
+        filter?.setValue(ciImage, forKey: kCIInputImageKey)
+    }
+}
+
+extension ViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return filters.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? CustomCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        cell.label.text = filters[indexPath.item]
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+     
+        currentFilter = filters[indexPath.item]
+    }
+    
+}
