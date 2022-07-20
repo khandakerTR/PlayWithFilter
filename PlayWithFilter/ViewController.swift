156b41042325b16f9d275b3f023b4b0e0c155090
Tushar Khandaker <khandaker3047@gmail.com>
2022-07-21 01:43:16 +0600

    Desing Updated

--- PlayWithFilter/ViewController.swift
+++ PlayWithFilter/ViewController.swift
@@ -21,20 +21,48 @@ class ViewController: UIViewController {
     var context = CIContext()
     var currentFilter = "CISepiaTone"
     var currentImage = UIImage()
+    var currentSliderValue = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
       
         currentImage = UIImage(named: "sample.jpg", in: Bundle(for: type(of: self)), compatibleWith: nil)!
     }
+    
+    @IBAction func didChangedSlideValue(_ sender: UISlider) {
+        currentSliderValue = Double(sender.value)
+        applyFilter()
+    }
+    
+    @IBAction func didTappedOnImportButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func didTappedOnSaveButton(_ sender: UIButton) {
+    }
+    
+    
 }
 
 extension ViewController {
     
     func applyFilter() {
         let ciImage = CIImage(image: currentImage)
-        let filter = CIFilter(name: currentFilter)
-        filter?.setValue(ciImage, forKey: kCIInputImageKey)
+        guard let filter = CIFilter(name: currentFilter) else { return }
+        filter.setValue(ciImage, forKey: kCIInputImageKey)
+        
+        
+        let inputKeys = filter.inputKeys
+        
+        if inputKeys.contains(kCIInputIntensityKey) { filter.setValue(currentSliderValue, forKey: kCIInputIntensityKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { filter.setValue(currentSliderValue * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { filter.setValue(currentSliderValue * 10, forKey: kCIInputScaleKey) }
+        if inputKeys.contains(kCIInputCenterKey) { filter.setValue(CIVector(x: currentImage.size.width / 2, y: currentImage.size.height / 2), forKey: kCIInputCenterKey) }
+        
+        if let cgimg = context.createCGImage(filter.outputImage!, from: filter.outputImage!.extent) {
+            let processedImage = UIImage(cgImage: cgimg)
+            self.imageView.image = processedImage
+            
+        }
     }
 }
 
@@ -56,8 +84,10 @@ extension ViewController: UICollectionViewDataSource {
 extension ViewController: UICollectionViewDelegate {
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-     
+        if currentFilter == filters[indexPath.item] {
+            return
+        }
         currentFilter = filters[indexPath.item]
+        applyFilter()
     }
-    
 }
